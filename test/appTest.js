fe08741dce7a29b6dbfde302693e4b270450e414
Ramzes Drugi <matej_jukic@hotmail.com>
2019-03-22 08:31:06 +0100

    Apples and oranges solved with TDD

--- test/appTest.js
+++ test/appTest.js
@@ -3,9 +3,13 @@ const app = require ('../app');
 
 describe('App', function(){
 
-    it('should return Hello World', function(){
 
-            assert.equal(app(), "Hello World");
+    it('should pass example', function(){
+
+            assert.deepEqual(app.countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]), [1, 1]);
 
     });
+
+   
 });
+
